e0ed31af22fb2b63cdd6dc1a35c6f6d7c68c0ddc
wodahS21 <yaelelyolo@gmail.com>
2023-09-29 09:25:16 -0600

    Instruccion frontend

--- hola_mundo.js
+++ hola_mundo.js
@@ -1,4 +1,9 @@
-//Mostrar texto
+//Imprime en pantalla
 console.log('hola mundo esto se vera en Node JS Esto se vera en la terminal de comandos');
 //Operacion Matematica
 console.log(2*5);
+//Esta linea nos dara error al ser un comando de frontend
+console.log(window)
+//
+
+//
